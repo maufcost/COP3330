a7479e6f6e48a3707f7d3fc8ff91fed2573d0295
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-01-12 15:47:42 -0500

    My first commit

---
+++ costa_01/src/costa_01/Example01.java
@@ -0,0 +1,17 @@
+package costa_01;
+
+import java.util.Scanner;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter your name: ");
+		String name = in.next();
+		
+		System.out.println("Hello, " + name);
+	}
+
+}
